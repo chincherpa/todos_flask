2c53f67d5335b011d8125aef28e316602a4eb4e0
x123069 <extern.lutz.collum@porsche.de>
2020-01-08 10:39:30 +0100

    *edit todo comments *layout

--- flaskapp/forms.py
+++ flaskapp/forms.py
@@ -5,4 +5,15 @@ from wtforms.validators import DataRequired
 
 class PostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
+    comment_1 = StringField('comment_1')
+    comment_2 = StringField('comment_2')
+    comment_3 = StringField('comment_3')
+    comment_4 = StringField('comment_4')
+    comment_5 = StringField('comment_5')
+    comment_6 = StringField('comment_6')
+    comment_7 = StringField('comment_7')
+    comment_8 = StringField('comment_8')
+    comment_9 = StringField('comment_9')
+    comment_10 = StringField('comment_10')
+    new_comment = StringField('new_comment')
     submit = SubmitField('ok')
--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -15,8 +15,10 @@ path_to_js = os.path.join(APP_STATIC, "todos.json")
 
 
 def dump_todos_to_json():
+    print("saving...")
     with open(path_to_js, "w") as f:
         json.dump(todos, f, indent=4)
+    print("saved!")
 
 
 def load_json():
@@ -65,25 +67,63 @@ def one_comments(todo_id, status):
 
 
 @app.route("/todo/<int:todo_id>/edit", methods=['GET', 'POST'])
+@app.route("/edit_todo/<int:todo_id>", methods=['GET', 'POST'])
 def edit_todo(todo_id):
+    print(f"{request.method=}")
     todo_id = str(todo_id)
     todo_dic = todos["todos"].get(todo_id)
-    print('#####################')
-    print('#####################')
-    print(todo_dic)
-    print('#####################')
-    print('#####################')
     form = PostForm()
-    if form.validate_on_submit():
-        print('*POST* ' * 7)
-        print(f'{form.title.data=}')
-        todo_dic["title"] = form.title.data
+    print(1)
+    # if form.validate_on_submit():
+    if request.method == "POST":
+        today = str(datetime.date.today())
+
+        todos["todos"][todo_id]["title"] = form.title.data
+        if form.comment_1.data:
+            todos["todos"][todo_id]["comments"][0][0] = form.comment_1.data
+        if form.comment_2.data:
+            todos["todos"][todo_id]["comments"][1][0] = form.comment_2.data
+        if form.comment_3.data:
+            todos["todos"][todo_id]["comments"][2][0] = form.comment_3.data
+        if form.comment_4.data:
+            todos["todos"][todo_id]["comments"][3][0] = form.comment_4.data
+        if form.comment_5.data:
+            todos["todos"][todo_id]["comments"][4][0] = form.comment_5.data
+        if form.comment_6.data:
+            todos["todos"][todo_id]["comments"][5][0] = form.comment_6.data
+        if form.comment_7.data:
+            todos["todos"][todo_id]["comments"][6][0] = form.comment_7.data
+        if form.comment_8.data:
+            todos["todos"][todo_id]["comments"][7][0] = form.comment_8.data
+        if form.comment_9.data:
+            todos["todos"][todo_id]["comments"][8][0] = form.comment_9.data
+        if form.comment_10.data:
+            todos["todos"][todo_id]["comments"][9][0] = form.comment_10.data
+
+        # new_comment
+        if form.new_comment.data:
+            todos["todos"][todo_id]["comments"].append([form.new_comment.data, today])
+
+        dump_todos_to_json()
         return redirect('/open')
 
     elif request.method == "GET":
-        print('*GET* ' * 7)
-        print(f'{todo_dic["title"]=}')
         form.title.data = todo_dic["title"]
+        lcommentdata = [
+            form.comment_1.data,
+            form.comment_2.data,
+            form.comment_3.data,
+            form.comment_4.data,
+            form.comment_5.data,
+            form.comment_6.data,
+            form.comment_7.data,
+            form.comment_8.data,
+            form.comment_9.data,
+            form.comment_10.data,
+        ]
+        if len(todo_dic["comments"]) > 1:
+            for i, c in enumerate(todo_dic["comments"]):
+                lcommentdata[i] = c[0]
 
     # return render_template('edit.html', todo=todo_dic)
     return render_template("edit.html", form=form, todo=todo_dic)
@@ -105,7 +145,7 @@ def reopen_todo(todo_id):
     return redirect('/open')
 
 
-@app.route("/new_todo")
+@app.route("/new_todo", methods=['POST'])
 def create_todo():
     today = str(datetime.date.today())
 
@@ -116,8 +156,6 @@ def create_todo():
     lnew_tags = list(filter(None, lnew_tags_temp))
 
     new_todo_reminder = request.form['new_todo_reminder']
-    # if not new_todo_reminder:
-    #     new_todo_reminder = ""
 
     todos["ids"] += 1
     new_id = str(todos["ids"])
@@ -131,7 +169,7 @@ def create_todo():
         "date_added": today,
         "rem_time": new_todo_reminder,
     }
-    # dump_todos_to_json()
+    dump_todos_to_json()
     print(todos["todos"][new_id])
     return redirect('/open')
 
